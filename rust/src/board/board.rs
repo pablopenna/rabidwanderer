6ef0f5ad0aa28cb1caa1a33b06b3890cb5796621
Pablo Pena <pablo_penna7@hotmail.com>
2025-08-05 21:01:57 +0800

    feat: add data tiles to board

--- rust/src/board/board.rs
+++ rust/src/board/board.rs
@@ -7,17 +7,15 @@ use rand::Rng;
 
 use crate::board::board_utils;
 
-const BOARD_WIDTH: i32 = 20;
-const BOARD_HEIGHT: i32 = 10;
-
-const TILE_SET_SOURCE_ID: i32 = 0;
-const TILE_SET_ALTERNATIVE_TILE: i32 = 0;
-const TILE_SET_FOURWAY_ATLAS_COORD: Vector2i = Vector2i::from_tuple((1, 0));
+const BOARD_WIDTH: usize = 20;
+const BOARD_HEIGHT: usize = 10;
+const BOARD_SIZE: usize = BOARD_WIDTH * BOARD_HEIGHT;
 
 #[derive(GodotClass)]
 #[class(base=TileMapLayer)]
 struct Board {
     random_generator: ThreadRng,
+    data: [DataTile; BOARD_SIZE],
     base: Base<TileMapLayer>,
 }
 
@@ -26,13 +24,14 @@ impl ITileMapLayer for Board {
     fn init(base: Base<TileMapLayer>) -> Self {
         Self {
             random_generator: rand::rng(),
+            data: generate_empty_board_data(),
             base,
         }
     }
 
     fn ready(&mut self) {
         board_utils::verify_tile_set_exists(self.base().to_godot());
-        populate_board(self);
+        Board::populate_board(self);
     }
 
     fn physics_process(&mut self, _delta: f64) {}
@@ -44,32 +43,100 @@ impl ITileMapLayer for Board {
     }
 }
 
-fn populate_board(board: &mut Board) {
-    let blocks_to_place: u8 = 100;
-    let mut blocks_placed: u8 = 0;
+#[godot_api] // https://godot-rust.github.io/book/register/functions.html#user-defined-functions
+impl Board {
+    fn populate_board(board: &mut Board) {
+        let blocks_to_place: u8 = 100;
+        let mut blocks_placed: u8 = 0;
 
-    while blocks_placed < blocks_to_place {
-        let x = board.random_generator.random_range(0..BOARD_WIDTH);
-        let y = board.random_generator.random_range(0..BOARD_HEIGHT);
-        let coord = Vector2i::from_tuple((x, y));
-        if is_cell_empty(board, coord) {
-            add_four_way_cell(board, coord);
-            blocks_placed += 1;
+        while blocks_placed < blocks_to_place {
+            let x = board.random_generator.random_range(0..BOARD_WIDTH) as i32;
+            let y = board.random_generator.random_range(0..BOARD_HEIGHT) as i32;
+            let coord = Vector2i::from_tuple((x, y));
+            if Self::is_cell_empty(board, coord) {
+                Self::add_four_way_tile(board, coord);
+                blocks_placed += 1;
+            }
         }
+    }
+
+    fn is_cell_empty(board: &mut Board, coordinates: Vector2i) -> bool {
+        let cell = board.base().get_cell_tile_data(coordinates);
+        return cell.is_none();
+    }
+
+    fn add_four_way_tile(board: &mut Board, coordinates: Vector2i) {
+        Self::add_draw_tile(board, coordinates, FOUR_WAY_DRAW_CELL);
+
+        let data_tile = &mut board.data[coordinate_to_index(godot_vector_to_vector2d(coordinates))];
+        data_tile.make_traversable();
+    }
+
+    fn add_draw_tile(board: &mut Board, coordinates: Vector2i, tile: DrawTile) {
+        board
+            .base_mut()
+            .set_cell_ex(coordinates)
+            .source_id(tile.source_id)
+            .atlas_coords(tile.atlas_coords)
+            .alternative_tile(tile.alternative_tile)
+            .done();
     }
 }
 
-fn is_cell_empty(board: &mut Board, coordinates: Vector2i) -> bool {
-    let cell = board.base().get_cell_tile_data(coordinates);
-    return cell.is_none();
+struct DrawTile {
+    source_id: i32,
+    atlas_coords: Vector2i,
+    alternative_tile: i32,
 }
 
-fn add_four_way_cell(board: &mut Board, coordinates: Vector2i) {
-    board
-        .base_mut()
-        .set_cell_ex(coordinates)
-        .source_id(TILE_SET_SOURCE_ID)
-        .atlas_coords(TILE_SET_FOURWAY_ATLAS_COORD)
-        .alternative_tile(TILE_SET_ALTERNATIVE_TILE)
-        .done();
+const FOUR_WAY_DRAW_CELL: DrawTile = DrawTile {
+    // coordinates: Vector2i::from_tuple((0, 0)),
+    source_id: 0,
+    alternative_tile: 0,
+    atlas_coords: Vector2i::from_tuple((1, 0)),
+};
+
+// =======
+// Data
+// =======
+struct Vector2D {
+    x: usize,
+    y: usize,
+}
+
+struct DataTile {
+    coordinates: Vector2D,
+    can_be_traversed: bool,
+}
+
+impl DataTile {
+    fn make_traversable(&mut self) {
+        self.can_be_traversed = true;
+    }
+}
+
+fn godot_vector_to_vector2d(vector: Vector2i) -> Vector2D {
+    Vector2D {
+        x: vector.x as usize,
+        y: vector.y as usize,
+    }
+}
+
+fn index_to_coordinate(idx: usize) -> Vector2D {
+    Vector2D {
+        x: idx % BOARD_WIDTH,
+        y: idx / BOARD_WIDTH,
+    }
+}
+
+fn coordinate_to_index(coordinate: Vector2D) -> usize {
+    coordinate.y * BOARD_WIDTH + coordinate.x
+}
+
+fn generate_empty_board_data() -> [DataTile; BOARD_SIZE as usize] {
+    let data: [DataTile; BOARD_SIZE as usize] = core::array::from_fn(|i| DataTile {
+        coordinates: index_to_coordinate(i),
+        can_be_traversed: false,
+    });
+    data
 }
