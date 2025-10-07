06014de7b3e385d2110c28d9c680ba152afc86c1
Pablo Pena <pablo_penna7@hotmail.com>
2025-10-07 21:09:59 +0800

    feat: prevent tooltip from crossing the screen limits

--- godot/scripts/tooltip.gd
+++ godot/scripts/tooltip.gd
@@ -3,9 +3,21 @@ extends PanelContainer
 # PopupPanel - https://www.youtube.com/watch?v=Q8oee9tRMSc
 # Panel + RichText Label - https://www.youtube.com/watch?v=6OyPgL2Elpw
 
-const OFFSET: Vector2 = Vector2.ONE * 20.0
+const OFFSET: Vector2 = Vector2.ONE * 5.0
 
 func _input(event: InputEvent) -> void:
 	if visible and event is InputEventMouseMotion:
 		global_position = get_global_mouse_position() + OFFSET
-		
+		if is_out_of_screen(): 
+			adjust()
+
+func is_out_of_screen() -> bool:
+	var view_rect: Rect2 = get_viewport_rect()
+	var tooltip_rect: Rect2 = self.get_global_rect()
+	return not view_rect.encloses(tooltip_rect)
+
+func adjust() -> void:
+	var max_pos: Vector2 = get_viewport_rect().size - self.get_rect().size
+	var min_pos: Vector2 = Vector2.ZERO
+	self.global_position.x = clamp(self.global_position.x, min_pos.x, max_pos.x)
+	self.global_position.y = clamp(self.global_position.y, min_pos.y, max_pos.y)
