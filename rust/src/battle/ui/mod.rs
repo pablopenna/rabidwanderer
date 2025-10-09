4a086bbf6296d75e3299750d60a3b3d1c1cea0bc
Pablo Pena <pablo_penna7@hotmail.com>
2025-10-09 21:01:35 +0800

    refactor: move tooltip trigger todedicated folder

--- rust/src/battle/ui/mod.rs
+++ rust/src/battle/ui/mod.rs
@@ -2,4 +2,3 @@ pub(crate) mod ui;
 pub(crate) mod hp_bar;
 pub(crate) mod skill_selector_container;
 pub(crate) mod skill_button;
-pub(crate) mod tooltip_trigger;
--- rust/src/ui/mod.rs
+++ rust/src/ui/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod link_inventory_to_ui;
 pub(crate) mod inventory_ui_display;
 pub(crate) mod item_details_ui;
+pub(crate) mod tooltip;
