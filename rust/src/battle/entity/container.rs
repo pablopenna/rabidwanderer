d35fe9b611be58822e2e2e0aa5c4b6bbb5f584c4
Pablo Pena <pablo_penna7@hotmail.com>
2025-11-18 19:14:38 +0800

    doc: add comments to BattleEntityContainer on why entities are stored as children instead of in an array as references

--- rust/src/battle/entity/container.rs
+++ rust/src/battle/entity/container.rs
@@ -4,6 +4,14 @@ use godot::prelude::*;
 use crate::battle::entity::entity::BattleEntity;
 use crate::battle::team::Team;
 
+/*
+ * BEs (Battle Entities) are stored as children instead of as references in an array.
+ * While the latter would be better in theory, we need to store them as children because
+ * the battle node tree is placed within a subviewport and thus in a different World2D.
+ * 
+ * Apparently, you cannot change a CanvasItem World2D. That is just decided from where they are on the tree.
+ * We need them to be in the battle World2D so they can be displayed in the Battle UI window.
+ */
 #[derive(GodotClass)]
 #[class(init, base=Node)]
 pub(crate) struct BattleEntityContainer {
@@ -38,6 +46,7 @@ impl BattleEntityContainer {
         self.base_mut().add_child(entity);
     }
 
+    /** Removes the entities as children but does not free them so they are still in memory */
     pub(crate) fn remove_all_entities(&mut self) {
         let children = self.base_mut().get_children();
         children.iter_shared().for_each(
